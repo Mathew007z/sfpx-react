047ef345eb960b52c45d58b0195a1887e38870db
Matias <matias.acuna@arkanosoft.com>
2023-08-31 17:52:51 -0300

    push

--- src/services/SPODataProvider.ts
+++ src/services/SPODataProvider.ts
@@ -1,6 +1,12 @@
 import { sp } from "@pnp/sp/presets/all";
 import "@pnp/sp/fields";
 
+export interface DocumentItem {
+  Id: number;
+  FileLeafRef: string;
+  Aliados: string;
+}
+
 export default class SPODataProvider {
   constructor(siteUrl: string) {
     sp.setup({
@@ -13,15 +19,48 @@ export default class SPODataProvider {
     });
   }
 
-  public async GetElement() {
+  public async GetElement(): Promise<DocumentItem[]> {
     try {
-      const items = await sp.web.lists
-        .getByTitle("pruebadrfi")
-        .items.select("Id", "Title", "Aliados")
-        .get();
+      const libraryName = "pruebaDocumentos";
+      const items = await sp.web.lists.getByTitle(libraryName).items.select("Id", "FileLeafRef", "Aliados").getAll();
       return items;
     } catch (error) {
       console.log(error);
+      return [];
     }
   }
 }
+
+
+
+
+
+// Get a lista de sp
+
+// import { sp } from "@pnp/sp/presets/all";
+// import "@pnp/sp/fields";
+
+// export default class SPODataProvider {
+//   constructor(siteUrl: string) {
+//     sp.setup({
+//       sp: {
+//         headers: {
+//           Accept: "application/json;odata=verbose",
+//         },
+//         baseUrl: siteUrl,
+//       },
+//     });
+//   }
+
+//   public async GetElement() {
+//     try {
+//       const items = await sp.web.lists
+//         .getByTitle("pruebadrfi")
+//         .items.select("Id", "Title", "Aliados")
+//         .get();
+//       return items;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
--- src/webparts/spfxFilterReact/components/SpfxFilterReact.tsx
+++ src/webparts/spfxFilterReact/components/SpfxFilterReact.tsx
@@ -1,9 +1,11 @@
+
 import * as React from 'react';
 import { ISpfxFilterReactProps } from './ISpfxFilterReactProps';
 import SPODataProvider from '../../../services/SPODataProvider';
 import { TextField } from 'office-ui-fabric-react';
+import { DocumentItem } from '../../../services/SPODataProvider';// Asegúrate de importar correctamente la interfaz
 
-export default class SpfxFilterReact extends React.Component<ISpfxFilterReactProps, { selectedAliado: string; documents: any[] }> {
+export default class SpfxFilterReact extends React.Component<ISpfxFilterReactProps, { selectedAliado: string; documents: DocumentItem[] }> {
   private dataProvider: SPODataProvider;
 
   constructor(props: ISpfxFilterReactProps) {
@@ -45,10 +47,79 @@ export default class SpfxFilterReact extends React.Component<ISpfxFilterReactPro
         />
         {filteredDocuments.map((doc) => (
           <div key={doc.Id} style={{padding:'40px', border:'1px solid #000'}}>
-            <h3>{doc.Title}</h3>
+            <h3>{doc.FileLeafRef}</h3>
           </div>
         ))}
       </div>
     );
   }
 }
+
+
+
+
+
+// // Prueba con Lista de sharepoit
+
+// import * as React from 'react';
+// import { ISpfxFilterReactProps } from './ISpfxFilterReactProps';
+// import SPODataProvider from '../../../services/SPODataProvider';
+// import { TextField } from 'office-ui-fabric-react';
+
+// export default class SpfxFilterReact extends React.Component<ISpfxFilterReactProps, { selectedAliado: string; documents: any[] }> {
+//   private dataProvider: SPODataProvider;
+
+//   constructor(props: ISpfxFilterReactProps) {
+//     super(props);
+//     this.dataProvider = new SPODataProvider("https://cafcon.sharepoint.com/sites/PeopleSearch");
+//     this.state = {
+//       selectedAliado: "",
+//       documents: []
+//     };
+//   }
+
+//   async componentDidMount() {
+//     try {
+//       const allFiles = await this.dataProvider.getFilesRecursively("/sites/PeopleSearch/pruebaDocumentos");
+//       this.setState({ documents: allFiles });
+//       console.log('log all files',allFiles);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+
+//   handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     this.setState({ selectedAliado: event.target.value });
+//   };
+
+//   render() {
+//     const { documents, selectedAliado } = this.state;
+
+//     // Filtrar documentos basados en el aliado seleccionado
+//     const filteredDocuments = documents.filter(doc =>
+//       doc.Aliados.toLowerCase().includes(selectedAliado.toLowerCase())
+//     );
+
+//     return (
+//       <div>
+//         <TextField
+//           placeholder='Buscar Documento por Aliado'
+//           onChange={this.handleSearchChange}
+//           value={selectedAliado}
+//         />
+//         <div style={{display:'flex', alignItems:'center',flexWrap:'wrap', justifyContent:'space-evenly'}}>
+//         {filteredDocuments.map((doc) => (
+//           <div key={doc.Id} style={{
+//             padding:'40px',
+//              border:'1px solid #000',
+//              width:'250px',
+//              margin:'15px'
+//              }}>
+//             <h3>{doc.FileLeafRef}</h3>
+//           </div>
+//         ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
